d73ea6806a16a9152b7ee5e13adcf720e375c5c5
keithwilliams <keith.t.williams10@gmail.com>
2025-08-10 01:28:46 -0400

    Create initial framework 2

---
+++ GreatTreeProject/GreatTreeWebParent/GreatTreeBackEnd/src/main/java/com/greattree/admin/GreatTreeBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.greattree.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatTreeBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreatTreeBackEndApplication.class, args);
+	}
+
+}
