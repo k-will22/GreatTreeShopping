51fb91f20e02299af18b5e8f443773fb260aa20c
keithwilliams <keith.t.williams10@gmail.com>
2025-08-11 22:37:43 -0400

    Add roles to database

--- GreatTreeProject/GreatTreeWebParent/GreatTreeBackEnd/src/main/java/com/greattree/admin/GreatTreeBackEndApplication.java
+++ GreatTreeProject/GreatTreeWebParent/GreatTreeBackEnd/src/main/java/com/greattree/admin/GreatTreeBackEndApplication.java
@@ -2,8 +2,10 @@ package com.greattree.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.greattree.common.entity", "com.greattree.admin.user"})
 public class GreatTreeBackEndApplication {
 
 	public static void main(String[] args) {
